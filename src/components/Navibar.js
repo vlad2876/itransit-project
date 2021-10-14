17e06121302ca1947732be792efbbfee8e2ad41a
vlad2876 <vlad28761@gmail.com>
2021-10-14 10:47:16 +0300

    .

--- src/components/Navibar.js
+++ src/components/Navibar.js
@@ -1,20 +1,13 @@
 import React, {useContext} from 'react';
 import {Navbar, Nav, Button} from "react-bootstrap";
 import {ADMIN_ROUTE, CREATE_TASK_ROUTE, HOMEPAGE_ROUTE, PROFILE_ROUTE} from "../utils/consts";
-import {Context} from "../index";
-import firebase from "firebase/compat";
-
+import {facebookProvider, googleProvider} from "../config/authMethods";
+import socialMediaAuth from "../service/auth";
 
 export default function NaviBar() {
-
-    const {auth} = useContext(Context)
-
-    const login = async () => {
-      const provider = new firebase.auth.FacebookAuthProvider()
-        const {user} = await auth.signInWithPopup(provider)
-    }
-
-
+    const handleOnClick = async (provider) => {
+        const res = await socialMediaAuth(provider)
+    };
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
@@ -26,8 +19,9 @@ export default function NaviBar() {
                     <Nav.Link href={PROFILE_ROUTE}>Profile</Nav.Link>
                 </Nav>
                 <Nav>
-                    <Button variant="primary" >VK</Button>
-                    <Button onClick={login}>Facebook</Button>
+                    <Button onClick={()=> handleOnClick(facebookProvider)}>Facebook</Button>
+                    <Button onClick={()=> handleOnClick(googleProvider)}>Google</Button>
+
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
--- src/index.js
+++ src/index.js
@@ -1,32 +1,9 @@
 import React, {createContext} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import firebase from 'firebase/compat';
-import 'firebase/firestore'
-import 'firebase/auth'
-
-firebase.initializeApp({
-    apiKey: "AIzaSyAQNYXmztXekA2Db4qQ4W9h9xCqvfYsADY",
-    authDomain: "itransit-37a98.firebaseapp.com",
-    projectId: "itransit-37a98",
-    storageBucket: "itransit-37a98.appspot.com",
-    messagingSenderId: "383979156170",
-    appId: "1:383979156170:web:aafa8bbc1f382e8986ba33",
-    measurementId: "G-T79FR0EZV1"});
-
-export const Context = createContext(null)
-
-const auth = firebase.auth()
-const firestore = firebase.firestore()
 
 ReactDOM.render(
-    <Context.Provider value={{
-        firebase,
-        auth,
-        firestore
-    }}>
-        <App />
-    </Context.Provider>,
+        <App />,
   document.getElementById('root')
 );
 
