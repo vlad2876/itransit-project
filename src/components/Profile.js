4f586fef51d27ebe353f741d1f6a48f08e29a64e
vlad2876 <vlad28761@gmail.com>
2021-10-15 03:14:41 +0300

    .

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,7 +15,7 @@ const Profile = () => {
             <div>
                 <div className="d-flex align-items-center justify-content-start">
                     <img className="avatar" src={user.photoURL}/>
-                    <p className="ms-3">{user.displayName}</p>
+                    <h2 className="ms-3">{user.displayName}</h2>
                 </div>
                 <Table striped bordered hover className="w-50 mt-4">
                     <thead>
