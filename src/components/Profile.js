e1c4211bbae23cd0a1e45b75e2574cdeb0873a60
vlad2876 <vlad28761@gmail.com>
2021-10-17 03:15:22 +0300

    .

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,88 +3,21 @@ import {Context} from "../index";
 import {useAuthState} from "react-firebase-hooks/auth";
 import {Redirect} from "react-router-dom";
 import {HOMEPAGE_ROUTE} from "../utils/consts";
-import {Table, Thead, Tbody, Tr, Th, Td} from 'react-super-responsive-table'
-import firebase from "../config/firebase-config";
-import 'react-super-responsive-table/dist/SuperResponsiveTableStyle.css';
+import TableItem from "./TableItem";
 
 const Profile = () => {
     const {auth} = useContext(Context)
     const [user] = useAuthState(auth)
-    let db = firebase.firestore();
-
-    const taskTable = document.querySelector('#task-table');
-
-    function addToTaskList(doc) {
-        let Tr = document.createElement('Tr');
-        let theme = document.createElement('Td');
-        let title = document.createElement('Td');
-        let tags = document.createElement('Td');
-        let task = document.createElement('Td');
-        let answer1 = document.createElement('Td');
-        let answer2 = document.createElement('Td');
-        let answer3 = document.createElement('Td');
-        let cross = document.createElement('div');
-
-        Tr.setAttribute('data-id', doc.id);
-        theme.textContent = doc.data().theme;
-        title.textContent = doc.data().title;
-        tags.textContent = doc.data().tags;
-        task.textContent = doc.data().task;
-        answer1.textContent = doc.data().answer1;
-        answer2.textContent = doc.data().answer2;
-        answer3.textContent = doc.data().answer3;
-        cross.textContent = 'x';
-        cross.className = "deleteButton"
-
-        Tr.appendChild(title);
-        Tr.appendChild(theme);
-        Tr.appendChild(tags);
-        Tr.appendChild(task);
-        Tr.appendChild(answer1);
-        Tr.appendChild(answer2);
-        Tr.appendChild(answer3);
-        Tr.appendChild(cross);
-        cross.addEventListener('click', (e) =>{
-            e.stopPropagation();
-            let id = e.target.parentElement.getAttribute('data-id');
-            db.collection('tasks').doc(id).delete();
-        })
-
-        taskTable.appendChild(Tr);
-    }
-
-    db.collection('tasks').get().then((snapshot) => {
-        snapshot.docs.forEach(doc => {
-            addToTaskList(doc)
-        })
-    })
 
     if (user) {
         return (
-
             <div>
                 <div className="d-flex align-items-center justify-content-start">
                     <img className="avatar" src={user.photoURL}/>
                     <h3 className="ms-3">{user.displayName}</h3>
                 </div>
-                <Table className="mt-5">
-                    <Thead>
-                        <Tr>
-                            <Th>Theme</Th>
-                            <Th>Title</Th>
-                            <Th>Task</Th>
-                            <Th>Tags</Th>
-                            <Th>Answer 1</Th>
-                            <Th>Answer 2</Th>
-                            <Th>Answer 3</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody id="task-table">
-
-                    </Tbody>
-                </Table>
+                    <TableItem/>
             </div>
-
         );
     } else {
         return (
---
+++ src/components/TableItem.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import firebase from "../config/firebase-config";
+import {Table} from "react-bootstrap";
+
+const TableItem = () => {
+    let db = firebase.firestore();
+
+    const taskTable = document.querySelector('#task-table');
+
+    function addToTaskList(doc) {
+        let tr = document.createElement('tr');
+        let theme = document.createElement('td');
+        let title = document.createElement('td');
+        let tags = document.createElement('td');
+        let task = document.createElement('td');
+        let answer1 = document.createElement('td');
+        let answer2 = document.createElement('td');
+        let answer3 = document.createElement('td');
+        let cross = document.createElement('div');
+
+        tr.setAttribute('data-id', doc.id);
+        theme.textContent = doc.data().theme;
+        title.textContent = doc.data().title;
+        tags.textContent = doc.data().tags;
+        task.textContent = doc.data().task;
+        answer1.textContent = doc.data().answer1;
+        answer2.textContent = doc.data().answer2;
+        answer3.textContent = doc.data().answer3;
+        cross.textContent = 'x';
+        cross.className = "deleteButton bg-danger"
+
+        tr.appendChild(title);
+        tr.appendChild(theme);
+        tr.appendChild(tags);
+        tr.appendChild(task);
+        tr.appendChild(answer1);
+        tr.appendChild(answer2);
+        tr.appendChild(answer3);
+        tr.appendChild(cross);
+        cross.addEventListener('click', (e) =>{
+            e.stopPropagation();
+            let id = e.target.parentElement.getAttribute('data-id');
+            db.collection('tasks').doc(id).delete();
+        })
+
+        taskTable.appendChild(tr);
+    }
+
+    db.collection('tasks').get().then((snapshot) => {
+        snapshot.docs.forEach(doc => {
+            addToTaskList(doc)
+        })
+    })
+
+    return (
+        <Table className="w-50 mt-3" striped bordered responsive>
+            <thead>
+            <tr>
+                <th>Theme</th>
+                <th>Title</th>
+                <th>Task</th>
+                <th>Tags</th>
+                <th>Answer 1</th>
+                <th>Answer 2</th>
+                <th>Answer 3</th>
+                <th className="deleteColumn"/>
+            </tr>
+            </thead>
+            <tbody id="task-table">
+
+            </tbody>
+        </Table>
+    );
+};
+
+export default TableItem;
